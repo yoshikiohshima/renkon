885311a41e93b18c7162ea690bf6bba671c79ef6
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2024-07-30 14:18:18 -0700

    build only es, not umd

--- vite.config-lib.ts
+++ vite.config-lib.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     build: {
         lib: {
           entry: resolve(__dirname, 'src/main.js'),
+          formats: ["es"],
           name: 'Renkon',
           fileName: 'renkon',
         },
