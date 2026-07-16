6cb9c1ef5385f827cab3ffa9887f04cb95872cc7
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2026-07-16 12:19:31 -0700

    generate d.ts file with vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         lib: {
-          entry: resolve(__dirname, 'src/main.js'),
+          entry: resolve(__dirname, 'src/main.ts'),
           formats: ["es"],
           name: 'Renkon-Core',
           fileName: 'renkon-core',
         },
         minify: false,
-        
-//         rollupOptions: {
-//            output: {
-//                manualChunks: {
-//                    typescript: ["typescript"]
-//                }
-//            }
-//        }
-    }
+    },
+    plugins: [dts({bundleTypes: true})]
 })
